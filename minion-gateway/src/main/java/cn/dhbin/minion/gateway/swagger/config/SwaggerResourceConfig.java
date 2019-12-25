a5617cb5d8ed7c251e3e3027f6580b9c24054a85
DHB <xx158@qq.com>
2019-12-25 15:23:05 +0800

    支持是否显示文档、修改标题

--- minion-gateway/src/main/java/cn/dhbin/minion/gateway/swagger/config/SwaggerResourceConfig.java
+++ minion-gateway/src/main/java/cn/dhbin/minion/gateway/swagger/config/SwaggerResourceConfig.java
@@ -3,6 +3,7 @@ package cn.dhbin.minion.gateway.swagger.config;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.context.annotation.Primary;
@@ -28,19 +29,31 @@ public class SwaggerResourceConfig implements SwaggerResourcesProvider {
 
     private static final String SWAGGER_VERSION = "2.0";
 
+    private static final String SWAGGER_ENABLED_KEY = "swagger";
+
+    private static final String SWAGGER_TITLE_KEY = "swagger_title";
+
 
     @Override
     public List<SwaggerResource> get() {
         List<SwaggerResource> resources = new ArrayList<>();
         List<String> routes = new ArrayList<>();
         routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
-        gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route -> {
-            route.getPredicates().stream()
-                    .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
-                    .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
-                            predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
-                                    .replace("**", "v2/api-docs"))));
-        });
+        gatewayProperties.getRoutes().stream()
+                .filter(
+                        routeDefinition -> routes.contains(routeDefinition.getId())
+                        && routeDefinition.getMetadata().entrySet().stream()
+                        .anyMatch(stringObjectEntry ->
+                              SWAGGER_ENABLED_KEY.equals(stringObjectEntry.getKey()) && "true".equals(stringObjectEntry.getValue())
+                        )
+                )
+                .forEach(
+                        route -> route.getPredicates().stream()
+                                .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
+                                .forEach(predicateDefinition -> resources.add(swaggerResource(determineSwaggerName(route),
+                                        predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
+                                                .replace("**", "v2/api-docs"))))
+                );
 
         return resources;
     }
@@ -53,4 +66,9 @@ public class SwaggerResourceConfig implements SwaggerResourcesProvider {
         swaggerResource.setSwaggerVersion(SWAGGER_VERSION);
         return swaggerResource;
     }
+
+    private String determineSwaggerName(RouteDefinition route) {
+        return route.getMetadata().containsKey(SWAGGER_TITLE_KEY) ? route.getMetadata().get(SWAGGER_TITLE_KEY).toString() : route.getId();
+    }
+
 }
