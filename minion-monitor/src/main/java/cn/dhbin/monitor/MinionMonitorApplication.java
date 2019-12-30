b488c773567016df77e4d13bfbb53647f9d5fb21
DHB <xx158@qq.com>
2019-12-30 15:25:29 +0800

    添加minion-monitor模块

---
+++ minion-monitor/src/main/java/cn/dhbin/monitor/MinionMonitorApplication.java
@@ -0,0 +1,21 @@
+package cn.dhbin.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 监控服务端
+ *
+ * @author donghaibin
+ * @date 2019/12/30
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class MinionMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinionMonitorApplication.class, args);
+    }
+
+}
