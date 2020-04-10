aaad6a264b987acfc2e7c97379cd64ec8b22313c
DHB <xx158@qq.com>
2020-04-10 12:43:56 +0800

    调整RemoteUserService提供者可空

--- minion-core/minion-core-security/src/main/java/cn/dhbin/core/security/component/MinionUserDetailServiceImpl.java
+++ minion-core/minion-core-security/src/main/java/cn/dhbin/core/security/component/MinionUserDetailServiceImpl.java
@@ -4,6 +4,7 @@ import cn.dhbin.minion.upms.Version;
 import cn.dhbin.minion.upms.dto.UserInfo;
 import cn.dhbin.minion.upms.service.RemoteUserService;
 import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,13 +17,18 @@ import org.springframework.stereotype.Component;
  * @date 2020/3/23
  */
 @Component
+@Slf4j
 public class MinionUserDetailServiceImpl implements UserDetailsService {
 
-    @Reference(version = Version.V_1_0_0)
+    @Reference(version = Version.V_1_0_0, check = false)
     private RemoteUserService remoteUserService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (remoteUserService == null) {
+            log.warn("remoteUserService == null，检查upms服务是否正常");
+            throw new UsernameNotFoundException("remoteUserService == null");
+        }
         UserInfo userInfo = remoteUserService.getByUsername(username);
         if (userInfo == null) {
             throw new UsernameNotFoundException(StrUtil.format("[{}] not found", username));
